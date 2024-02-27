ae4d7000b38b113794afe60035a20b6eff44d1c0
Oluwaseyifunmi Bamijoko <134262354+Bamijoko-Seyi@users.noreply.github.com>
2024-02-26 19:29:05 -0700

    Uploaded projects made since 2023

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool exit = false;
+        while (exit == false)
+        {
+            Console.Write("Please enter a number: ");
+            int user_input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= user_input; i++)
+            {
+                if (((i % 3) == 0) && ((i % 5) == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ((i % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.Write("Do you want to quit (y/n): ");
+            string input = Console.ReadLine();
+            if(input == "y")
+            {
+                exit = true;
+            }
+        }
+    }
+}
---
+++ MyfirstCsharp/MyfirstCsharp/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            bool Playagain = true;
+            while (Playagain)
+            {
+                while()
+            }
+
+            //This is a comment
+            /*
+             * this
+             * is
+             * a
+             * multiline
+             * comment
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
